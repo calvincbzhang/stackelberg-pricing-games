4579991763ae161e94070dc3b0ad5880988bc85a
Chen Bo Calvin Zhang <chenbocalvinzhang@gmail.com>
2021-05-02 14:50:49 +0100

    Debug linear regression and evaluation metrics

--- PlayerYaoMing.java
+++ PlayerYaoMing.java
@@ -17,10 +17,11 @@ import java.lang.Math;
 final class PlayerYaoMing extends PlayerImpl {
 
 	private ArrayList<Record> data = new ArrayList<Record>();
-	private ArrayList<Float> expectedUF = new ArrayList<Float>();
-	private float profitAccumulation = 0;
-	private float a_star;
-	private float b_star;
+	private ArrayList<Double> expectedUF = new ArrayList<Double>();
+	private double profitAccumulation = 0;
+	private double expectedProfit = 0;
+	private double a_star;
+	private double b_star;
 
 	PlayerYaoMing() throws RemoteException, NotBoundException {
 		super(PlayerType.LEADER, "Yao Ming Leader");
@@ -28,40 +29,34 @@ final class PlayerYaoMing extends PlayerImpl {
 
 	@Override
 	public void startSimulation(int p_steps) throws RemoteException {
-        int history_len = 100;
+		int history_len = 100;
 		
-        for (int t=0; t <= history_len; t++) {
+        for (int t=0; t < history_len; t++) {
             this.data.add(m_platformStub.query(this.m_type, t+1));
-			// String date = String.format("%d", this.data.get(t).m_date);
-			// String fP = String.format("%f", this.data.get(t).m_followerPrice);
-			// String lP = String.format("%f", this.data.get(t).m_leaderPrice);
-
-			// m_platformStub.log(this.m_type, date);
-			// m_platformStub.log(this.m_type, fP);
-			// m_platformStub.log(this.m_type, lP);
-        }
+		}
 	}
 	
 	@Override
 	public void endSimulation()	throws RemoteException {
-		this.data.add(m_platformStub.query(this.m_type, data.size()));
+		this.data.add(m_platformStub.query(this.m_type, data.size()+1));
+		profitAccumulation += computeProfitOnDay(data.size());
 
 		System.out.println("Final cumulative profit is " + profitAccumulation);
-		float mse = 0;
-		float mape = 0;
+		System.out.println("Expected cumulative profit is " + expectedProfit);
 
-		for (int t=101; t < data.size(); t++) {
+		double mse = 0;
+		double mape = 0;
+
+		for (int t=100; t < data.size(); t++) {
 			Record u_record = data.get(t);
-			float u_f_hat = expectedUF.get(t-101);
-			float u_f_true = u_record.m_followerPrice;
-			System.out.println("u_hat " + u_f_hat);
-			System.out.println("u_true " + u_f_true);
+			double u_f_hat = expectedUF.get(t-100);
+			double u_f_true = u_record.m_followerPrice;
 			mse += Math.pow(u_f_true - u_f_hat, 2);
 			mape += Math.abs((u_f_true - u_f_hat) / u_f_true);
 		}
 
-		mse /= (data.size() - 101);
-		mape /= (data.size() - 101);
+		mse /= (data.size() - 100);
+		mape /= (data.size() - 100);
 		
 		System.out.println("Mean Squared Error " + mse);
 		System.out.println("Mean Absolute Percentage Error " + mape);
@@ -70,27 +65,36 @@ final class PlayerYaoMing extends PlayerImpl {
 		this.data.clear();
 		this.expectedUF.clear();
 		this.profitAccumulation = 0;
+		this.expectedProfit = 0;
 	}
 
-	public float demandModel(float u_l, float u_f) {
-		return (float) (2 - u_l + 0.3 * u_f);
+	public double demandModel(double u_l, double u_f) {
+		return (double) (2 - u_l + 0.3 * u_f);
 	}
 
-	public float dailyProfit(float u_l, float u_f, float c) {
+	public double dailyProfit(double u_l, double u_f, double c) {
 		return (u_l - c) * demandModel(u_l, u_f);
 	}
 
+	public double computeProfitOnDay(int date) {
+		Record record = data.get(date - 1);
+		double real_ul = record.m_leaderPrice;
+		double real_uf = record.m_followerPrice;
+
+		return dailyProfit(real_ul, real_uf, 1);
+	}
+
 	public void estimateReaction() {
-		int T = data.size();
-		float sum_L = 0;
-		float sum_F = 0;
-		float sum_Lsq = 0;
-		float sum_LF = 0;
+		int T = this.data.size();
+		double sum_L = 0;
+		double sum_F = 0;
+		double sum_Lsq = 0;
+		double sum_LF = 0;
 
 		for (int t=0; t < T; t++) {
-			Record record = data.get(t);
-			float u_l_t = record.m_leaderPrice;
-			float u_f_t = record.m_followerPrice;
+			Record record = this.data.get(t);
+			double u_l_t = record.m_leaderPrice;
+			double u_f_t = record.m_followerPrice;
 
 			sum_L += u_l_t;
 			sum_F += u_f_t;
@@ -98,8 +102,8 @@ final class PlayerYaoMing extends PlayerImpl {
 			sum_LF += (u_l_t * u_f_t);
 		}
 
-		float a_star = ((sum_Lsq * sum_F) - (sum_L * sum_LF)) / ((T * sum_Lsq) - (sum_L * sum_L));
-		float b_star = ((T * sum_LF) - (sum_L * sum_F)) / ((T * sum_Lsq) - (sum_L * sum_L));
+		double a_star = ((sum_Lsq * sum_F) - (sum_L * sum_LF)) / ((T * sum_Lsq) - (sum_L * sum_L));
+		double b_star = ((T * sum_LF) - (sum_L * sum_F)) / ((T * sum_Lsq) - (sum_L * sum_L));
 
 		this.a_star = a_star;
 		this.b_star = b_star;
@@ -117,23 +121,22 @@ final class PlayerYaoMing extends PlayerImpl {
 		// update data array
 		if (p_date > 101) {
 			this.data.add(m_platformStub.query(this.m_type, p_date - 1));
+			profitAccumulation += computeProfitOnDay(p_date - 1);
 		}
 
 		// estimate the coefficient for the reaction function
 		estimateReaction();
-		System.out.println(this.a_star);
-		System.out.println(this.b_star);
+		// System.out.println(this.a_star);
+		// System.out.println(this.b_star);
 
 		// estimated reaction
-		float u_l = (float) (0.3 * (this.a_star - this.b_star) - 3) / (float) (2 * ((0.3 * this.a_star - 1)));
-		float u_f = (float) (this.a_star * u_l + this.b_star);
-		
+		double u_l = (0.3 * (this.a_star - this.b_star) - 3) / (2 * ((0.3 * this.a_star - 1)));
+		double u_f = (this.a_star * u_l + this.b_star);
+	
 		expectedUF.add(u_f);
+		expectedProfit += dailyProfit(u_l, u_f, 1);
 		
-		profitAccumulation += dailyProfit(u_l, u_f, 1);
-		System.out.println("Current cumulative profit is " + profitAccumulation);
-		
-		m_platformStub.publishPrice(m_type, u_l);
+		m_platformStub.publishPrice(m_type, (float) u_l);
 		
 	}
 
